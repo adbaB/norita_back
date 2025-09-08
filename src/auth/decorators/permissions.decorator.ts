6eb8c2cd2be45c147670470d772d2e2a4119b6c2
Alberto Domingo Basabe Acurero <alberto_12ba@hotmail.es>
2025-09-08 15:26:37 -0400

    remove old file

--- src/auth/decorators/permissions.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { CustomDecorator, SetMetadata } from '@nestjs/common';
-import { PermissionsEnum } from '../../libs/permissions.enum';
-
-export const PERMISSIONS_KEY = 'roles';
-export const Permissions = (...permissions: PermissionsEnum[]): CustomDecorator<string> =>
-  SetMetadata(PERMISSIONS_KEY, permissions);
