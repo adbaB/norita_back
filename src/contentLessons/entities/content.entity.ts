35dded6e43127c352b8a448138b1f22bb7b284a9
Alberto Domingo Basabe Acurero <alberto_12ba@hotmail.es>
2025-10-13 14:48:03 -0400

    add content lessons module and entities for bibliography, content, dialog, glossary, and notes

---
+++ src/contentLessons/entities/content.entity.ts
@@ -0,0 +1,71 @@
+import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Lesson } from '../../lessons/entities/lesson.entity';
+import { Bibliography } from './biblography.entity';
+import { Dialog } from './dialog.entity';
+import { Glossary } from './glossary.entity';
+import { Notes } from './notes.entity';
+
+@Entity('content')
+export class Content {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  content: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @OneToMany(() => Dialog, (dialog) => dialog)
+  dialogs: Dialog[];
+
+  @OneToMany(() => Notes, (notes) => notes)
+  notes: Notes[];
+
+  @OneToMany(() => Glossary, (glossary) => glossary)
+  glossaries: Glossary[];
+
+  @OneToMany(() => Bibliography, (bibliography) => bibliography)
+  bibliographies: Bibliography[];
+
+  //pending exercise entity
+  exercises: unknown[];
+
+  @OneToOne(() => Lesson, (lesson) => lesson.lessonContent)
+  @JoinColumn({ name: 'lesson_uuid' })
+  lesson: Lesson;
+
+  @Exclude({ toPlainOnly: true })
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Exclude({ toPlainOnly: true })
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @Exclude({ toPlainOnly: true })
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamptz',
+  })
+  deletedAt?: Date;
+}
---
+++ src/contentLessons/entities/glossary.entity.ts
@@ -0,0 +1,60 @@
+import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Content } from './content.entity';
+
+@Entity('glossary')
+export class Glossary {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'text' })
+  kanji: string;
+
+  @Column({ type: 'text' })
+  kana: string;
+
+  @Column({ type: 'text' })
+  romaji: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'varchar', nullable: true, length: 255 })
+  audio: string;
+
+  @ManyToOne(() => Content, (content) => content.glossaries)
+  @JoinColumn({ name: 'lesson_content_uuid' })
+  lessonContent: Content;
+
+  @Exclude({ toPlainOnly: true })
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Exclude({ toPlainOnly: true })
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @Exclude({ toPlainOnly: true })
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamptz',
+  })
+  deletedAt?: Date;
+}
