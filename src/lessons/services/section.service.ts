c55f4121d825dcb67f2957be3d53dd5c919694c0
Alberto Domingo Basabe Acurero <alberto_12ba@hotmail.es>
2025-11-09 12:39:29 -0400

    feat: implement CRUD operations for sections with role-based access control

--- src/lessons/services/section.service.ts
+++ src/lessons/services/section.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { MoreThanOrEqual, Repository } from 'typeorm';
+import { CreatedResponse } from '../../utils/responses';
+import { CreateSectionDto } from '../dto/section.dto';
 import { Section } from '../entities/section.entity';
 
 @Injectable()
 export class SectionService {
   constructor(@InjectRepository(Section) private sectionRepository: Repository<Section>) {}
+
+  async create(section: CreateSectionDto): Promise<CreatedResponse<Section>> {
+    const sections = await this.sectionRepository.find({
+      where: { order: MoreThanOrEqual(section.order) },
+    });
+
+    for (const sec of sections) {
+      sec.order += 1;
+      await this.sectionRepository.save(sec);
+    }
+
+    const newSection = await this.sectionRepository.save(section);
+
+    return {
+      status: 201,
+      message: 'Section created successfully',
+      data: newSection,
+    };
+  }
 
   async findAll(userUUID?: string): Promise<Section[]> {
     // return this.sectionRepository.find({ order: { order: 'ASC' }, relations: ['lessons'] });
@@ -32,5 +53,28 @@ export class SectionService {
 
   async findByUUID(uuid: string): Promise<Section> {
     return this.sectionRepository.findOne({ where: { uuid: uuid } });
+  }
+
+  async update(uuid: string, updateData: Partial<Section>): Promise<Section> {
+    const section = await this.sectionRepository.findOne({ where: { uuid } });
+
+    if (!section) {
+      throw new Error('Section not found');
+    }
+    const sections = await this.sectionRepository.find({
+      where: { order: MoreThanOrEqual(updateData.order) },
+    });
+
+    for (const sec of sections) {
+      if (sec.uuid !== uuid) {
+        sec.order += 1;
+        await this.sectionRepository.save(sec);
+      }
+    }
+    return this.sectionRepository.save({ ...section, ...updateData });
+  }
+
+  async remove(uuid: string): Promise<void> {
+    await this.sectionRepository.delete(uuid);
   }
 }
