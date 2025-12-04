6585b7ebbf89c99d67a482852a779f1a907ed70e
Alberto Domingo Basabe Acurero <alberto_12ba@hotmail.es>
2025-12-04 13:54:51 -0400

    change entitie library item add new module libraryType and add new entity Kana

--- src/library/entities/libraryItem.entity.ts
+++ src/library/entities/libraryItem.entity.ts
@@ -1,4 +1,4 @@
-import { Exclude, Expose } from 'class-transformer';
+import { Exclude } from 'class-transformer';
 import {
   Column,
   CreateDateColumn,
@@ -10,11 +10,6 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { IOrder } from '../../utils/interfaces/order.interface';
-import { LibraryAudio } from '../interfaces/audio.interface';
-import { ErrorItem } from '../interfaces/error.interface';
-import { Pronunciation } from '../interfaces/pronunciation.interface';
-import { Romanji } from '../interfaces/romanji.interface';
-import { StepImage } from '../interfaces/stepImage.interface';
 import { WordType } from '../interfaces/wordType.interface';
 import { LibrarySection } from './librarySection.entity';
 
@@ -23,32 +18,8 @@ export class LibraryItem implements IOrder {
   @PrimaryGeneratedColumn('uuid')
   uuid: string;
 
-  @Column({ type: 'jsonb', default: { male: '', female: '' } })
-  audio: LibraryAudio;
-
-  @Column({ type: 'jsonb', default: [] })
-  error: ErrorItem[];
-
-  @Column({ type: 'varchar', length: 255, nullable: true })
-  lottie: string;
-
   @Column({ type: 'varchar', length: 255, nullable: true })
   package: string;
-
-  @Column({ type: 'jsonb', default: [] })
-  pronunciation: Pronunciation[];
-
-  @Column({ type: 'jsonb', default: { hepburn: '', kunreishiki: '', nihonshiki: '' } })
-  romanji: Romanji;
-
-  @Column({ type: 'jsonb', default: [] })
-  images: StepImage[];
-
-  @Column({ name: 'final_image', type: 'varchar', length: 255, nullable: true })
-  finalImage: string;
-
-  @Column({ type: 'varchar', length: 255, nullable: true })
-  word: string;
 
   @Column({ type: 'jsonb', default: [] })
   wordType: WordType[];
@@ -59,11 +30,6 @@ export class LibraryItem implements IOrder {
   @ManyToOne(() => LibrarySection, (section) => section.items)
   @JoinColumn({ name: 'library_section_uuid' })
   section: LibrarySection;
-
-  @Expose({ name: 'steps' })
-  steps(): number {
-    return this.images.length;
-  }
 
   @Exclude({ toPlainOnly: true })
   @CreateDateColumn({
---
+++ src/libraryType/entities/kana.entity.ts
@@ -0,0 +1,42 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { LibraryAudio } from '../interfaces/commons/audio.interface';
+import { ErrorItem } from '../interfaces/commons/error.interface';
+import { Pronunciation } from '../interfaces/commons/pronunciation.interface';
+import { StepImage } from '../interfaces/commons/stepImage.interface';
+import { Romanji } from '../interfaces/kana/romanji.interface';
+
+@Entity('library_item_kana')
+export class Kana {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'jsonb', default: { male: '', female: '' } })
+  audio: LibraryAudio;
+
+  @Column({ type: 'jsonb', default: [] })
+  consonant: string[]; // TODO: define consonant interface if needed
+
+  @Column({ type: 'jsonb', default: [] })
+  error: ErrorItem[];
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  lottie: string;
+
+  @Column({ type: 'jsonb', default: [] })
+  pronunciation: Pronunciation[];
+
+  @Column({ type: 'jsonb', default: { hepburn: '', kunreishiki: '', nihonshiki: '' } })
+  romanji: Romanji;
+
+  @Column({ type: 'jsonb', default: [] })
+  images: StepImage[];
+
+  @Column({ type: 'varchar', length: 250, nullable: true })
+  steps: string;
+
+  @Column({ name: 'final_image', type: 'varchar', length: 255, nullable: true })
+  finalImage: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  word: string;
+}
