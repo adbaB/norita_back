13b52d474e4a8c0b42b0a6e33caa10fe3b90fcb3
Alberto Domingo Basabe Acurero <alberto_12ba@hotmail.es>
2025-12-14 16:14:13 -0400

    api beared Auth decorator

--- src/library/controllers/libraryItem.controller.ts
+++ src/library/controllers/libraryItem.controller.ts
@@ -9,7 +9,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { ApiBody, ApiParam, ApiResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiParam, ApiResponse } from '@nestjs/swagger';
 import {
   ApiResponse as ClassApiResponse,
   DeleteResponse,
@@ -19,6 +19,7 @@ import { CreateLibraryItemDTO, UpdateLibraryItemDTO } from '../dto/libraryItem.d
 import { LibraryItem } from '../entities/libraryItem.entity';
 import { LibraryItemService } from '../services/libraryItem.service';
 
+@ApiBearerAuth()
 @Controller('library/item')
 export class LibraryItemController {
   constructor(private readonly libraryItemService: LibraryItemService) {}
